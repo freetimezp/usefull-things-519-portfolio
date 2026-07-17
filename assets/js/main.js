3042c2ac055d83f816340f9cbe97daf875b629d0
Yevhen Onoprienko <fromzpcity@gmail.com>
2026-07-17 19:35:45 +0300

    05. usefull-things-519-portfolio. Typed.

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+var typed = new Typed("#typed", {
+    strings: ["Full-Stack Web Developer", "Database Developer", "UI/UX Designer"],
+    typeSpeed: 50,
+    backSpeed: 20,
+    backDelay: 3000,
+    showCursor: false,
+    loop: true,
+});
